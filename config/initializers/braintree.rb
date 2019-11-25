c3e17a376f594859827786b4b9a62fec6d1f9538
translatorzepp <3465940+translatorzepp@users.noreply.github.com>
2019-11-25 11:07:23 -0600

    Change name to BraintreeGraphQLRailsExample

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,5 @@
 Dotenv.load
 
 if !ENV["BT_PUBLIC_KEY"] || !ENV["BT_PRIVATE_KEY"] || !ENV["BT_VERSION"]
-  raise "Cannot find necessary environmental variables. See https://github.com/braintree/braintree_rails_example#setup-instructions for instructions";
+  raise "Cannot find necessary environmental variables. See https://github.com/braintree/braintree_graphql_rails_example#setup-instructions for instructions";
 end
